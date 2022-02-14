aeed031be4e3783220af351fc8daa67fdaa8e60f
Contreras, Erika <erika.contreras@accenture.com>
2022-02-13 22:01:53 -0600

    Curso java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Adios Mundo");
+       // int myVar;
+        //myVar = 50;
+
+        //System.out.println(myVar);//este es un comentario
+       //int anotherVar = 100;
+        //System.out.println(anotherVar);
+        int valA= 21;
+        int valB= 6;
+        int valC= 3;
+        int valD= 1;
+
+        int result1 = valA - valB / valC;
+        int result2 = (valA - valB) / valC;
+        System.out.println(result1);
+        System.out.println(result2);
     }
 
 }
